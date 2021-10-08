dfd32edb0cdb79219497593bcc08e671fad99d6f
katiebrown1092 <80285995+katiebrown1092@users.noreply.github.com>
2021-10-07 18:37:59 -0700

    logo update + JS Docs added

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+console.log("Your index.js file is loaded correctly!");
+
+$(function() {
+    $('.lazy').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+    
+});
+
