9b988b2c62bb9f2fb8d4227264f8108ce0d8ddf3
katiebrown1092 <80285995+katiebrown1092@users.noreply.github.com>
2021-10-09 10:38:33 -0700

    2nd try, excess Jquery files removed

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 
 console.log("Your index.js file is loaded correctly!");
 
-$(function() {
-    $('.lazy').Lazy({
-        scrollDirection: 'vertical',
-        effect: 'fadeIn',
-        visibleOnly: true,
-        onError: function(element) {
-            console.log('error loading ' + element.data('src'));
-        }
-    });
-    
-});
-
+$('#hero').load().delay(100).animate({'opacity':'1'},600);
