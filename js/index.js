fbb3362a99dc538488c16963fa71cf0eea6a8e5d
katiebrown1092 <80285995+katiebrown1092@users.noreply.github.com>
2021-10-11 20:24:04 -0700

    ScrollReveal Mastered

---
+++ js/index.js
@@ -0,0 +1,54 @@
+ScrollReveal().reveal('#header-logo', {
+    delay: 200,
+    duration:3000,
+    origin: 'top',
+    
+});
+ScrollReveal().reveal('#globalNav', { 
+    delay: 500,
+    origin: 'right',
+    duration:3000,
+    
+});
+
+ScrollReveal().reveal('#hero_image', { 
+    delay: 800,
+    origin: 'left',
+    duration:3000,
+    
+});
+ScrollReveal().reveal('.heroText', { 
+    delay: 900,
+    origin:'right',
+    duration:3000,
+    
+});
+ScrollReveal().reveal('.verticalLine', { 
+    delay: 700,
+    duration:3000,
+ 
+});
+
+ScrollReveal().reveal('#deptOfEd', { 
+    delay: 100,
+    duration:3000,
+    origin:'left'
+});  
+
+ScrollReveal().reveal('#pgk', { 
+    delay: 100,
+    duration:2000,
+    origin:'right'
+});  
+
+ScrollReveal().reveal('#govid', { 
+    delay: 100,
+    duration:3000,
+    origin:'left'
+});  
+
+ScrollReveal().reveal('#grnbox', { 
+    delay: 100,
+    duration:2000,
+    origin:'right'
+});  
